cc070239bc112ba8c0cd507379f6a77745955ac9
Hector DelaRosa <hdelaros@gmail.com>
2019-11-11 01:28:25 -0500

    addedUser functionality added and functional

--- app.js
+++ app.js
@@ -16,20 +16,62 @@ const readFilePromise = path => {
   });
 };
 
-http
-  .createServer((req, res) => {
+const writeFilePromise = (file, data) => {
+  return new Promise((res, rej) => {
+    fs.writeFile(file, data, (err, data) => {
+      if (err) {
+        rej(err);
+      } else {
+        res();
+      }
+    });
+  });
+};
+const addUser = user => {
+  return readFilePromise('./guests.json')
+    .then(data => {
+      const users = JSON.parse(data);
+      let max = users.reduce((acc, user) => {
+        if (user.id > acc) {
+          acc = user.id;
+        }
+        return acc;
+      }, 0);
+      user.id = max + 1;
+      users.push(user);
+      return writeFilePromise('./guests.json', JSON.stringify(users));
+    })
+    .then(() => {
+      return user;
+    });
+};
+
+http.createServer((req, res) => {
     if (req.url === '/api/guests') {
-      readFilePromise('./guests.json').then(data => {
+      if (req.method === 'GET') {
+        readFilePromise(path.join(__dirname, '/guests.json')).then(data => {
+          res.write(data);
+          res.end();
+        });
+      } else if (req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+          body += chunk;
+        });
+        req.on('end', () => {
+          
+          const user = JSON.parse(body);
+          addUser(user).then(data => {
+            res.statusCode = 201;
+            res.write(JSON.stringify(data));
+            res.end();
+          }).catch(ex => console.log(ex));
+        });
+      }
+    } else if (req.url === '/') {
+      readFilePromise(path.join(__dirname, '/index.html')).then(data => {
         res.write(data);
         res.end();
       });
     }
-
-    else if(req.url === '/') {
-        readFilePromise('./index.html').then(data => {
-            res.write(data);
-            res.end();
-          });
-    }
-  })
-  .listen(3000);
+  }).listen(3000);
